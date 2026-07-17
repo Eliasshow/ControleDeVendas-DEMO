6a5aad6439e379f6da4907080547431dec6fb285
Eliasshow <eliassamuel395@gmail.com>
2026-07-17 16:27:02 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://kekudywfeexhzlmmxslz.supabase.co'
-const supabaseAnonKey = 'sb_publishable_wLX2k7XUmO-z6UOcDYy2vg_R1ez0phL'
+const supabaseAnonKey = 'sb_publishable_Ah_kKA29qQiEbZMrKcVcew_Iyjk_4Zz'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
