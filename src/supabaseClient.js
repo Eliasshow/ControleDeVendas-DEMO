fe6b94d7c013ec1228437b32097126d67540d7c1
Eliasshow <eliassamuel395@gmail.com>
2026-07-20 13:51:18 -0300

    Atualizar chaves Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kekudywfeexhzlmmxslz.supabase.co'
-const supabaseAnonKey = 'sb_publishable_Ah_kKA29qQiEbZMrKcVcew_Iyjk_4Zz'
+const supabaseUrl = 'https://gnkgstjbtvcggojiraiz.supabase.co';
+const supabaseAnonKey = 'sb_publishable_Ah_kKA29qQiEbZMrKcVcew_Iyjk_4Zz';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
